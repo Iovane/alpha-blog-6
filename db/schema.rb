7a3798a84a2d70121573a448d7f1eabd546aaf84
Iovane <iovane.chikovani.1@btu.edu.ge>
2023-10-19 14:58:04 +0400

    added functionality to display articles with specific article id

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_10_091744) do
+ActiveRecord::Schema.define(version: 2023_10_10_093240) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.time "created_at"
     t.time "updated_at"
+    t.string "description"
   end
 
 end
